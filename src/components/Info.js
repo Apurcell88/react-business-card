69c1ab939590f263372ce11e18d1f60c4ec03397
Adam Purcell <apurcell88@gmail.com>
2022-11-11 16:36:20 -0600

    Added a bunch of styling.

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,13 +1,13 @@
 const Info = () => {
   return (
     <section className="info__container">
-      <img className="info__pic-me" src="C:/Users/apurc/OneDrive/Desktop/react/business-card/src/images/info-profile-pic.jpg" alt="me" />
+      <img className="info__pic-me" src="/images/person.jpg" alt="me" />
       <h1 className="info__name">Adam Purcell</h1>
       <h2 className="info__career">Frontend Engineer</h2>
       <h3 className="info__email">apurcell88@gmail.com</h3>
-      <article className="">
-        <button className="info__email-btn">Email</button>
-        <button>LinkedIn</button>
+      <article className="info__buttons-container">
+        <button className="info__email-btn info__btn">Email</button>
+        <button className="info__linkedin-btn info__btn">LinkedIn</button>
       </article>
     </section>  
   );
